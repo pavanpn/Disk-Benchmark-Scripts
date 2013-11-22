0a117ede54b68ce19e7afc79e21cf633b1559cb3
pavanpn <pavankumar.pn@gmail.com>
2013-11-21 18:07:17 -0800

    Version 2

--- fiolatval.pl
+++ fiolatval.pl
@@ -1,6 +1,6 @@
 #! /usr/bin/perl
 
-print "Script Started \n";
+print "FIO Started \n";
 my $result=`fio randread --output rrresults.txt`;
 my $out=`rm -rf /root/test`;
 
@@ -12,3 +12,20 @@ my $out=`rm -rf /root/test`;
 
 my $result=`fio seqwrite --output swresults.txt`;
 my $out=`rm -rf /root/test`;
+
+my $filename = 'filebench.txt';
+open(my $fh, '>>', $filename) or die "Could not open file '$filename' $!";
+
+print "Filebench Started \n";
+my $result=`filebench -f /root/Scripts/varmail.f`;
+my $out=`rm -rf /root/FTest/`;
+print $fh "$result";
+
+my $result=`filebench -f /root/Scripts/fileserver.f`;
+my $out=`rm -rf /root/FTest/`;
+print $fh "$result";
+
+my $result=`filebench -f /root/Scripts/webserver.f`;
+my $out=`rm -rf /root/FTest/`;
+print $fh "$result";
+close $fh;
